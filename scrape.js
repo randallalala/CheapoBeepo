b72de2f804081ff9993a2d28c299d6e48a97cb91
Randalla <the.randall.lim@gmail.com>
2020-07-23 00:14:47 +0800

    scrapescrape

--- scrape.js
+++ scrape.js
@@ -3,8 +3,122 @@ const cheerio = require("cheerio")
 const axios = require('axios')
 const fs = require("fs")
 const writeStream = fs.createWriteStream("post.csv")
+const convert = require('convert-units')
+
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 
+mongoose.Promise = Promise;
+
+mongoose
+.connect(process.env.MONGODBLIVE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+})
+.then(() => {
+  console.log("mongodb is running!");
+})
+.catch((e) => {
+  console.log(e);
+});
+
+const Item = require("./models/item.model");
+const { parse } = require("path")
+
+
+
+
+//axios will get the content
+axios.get("https://www.fairprice.com.sg/category/infant-formula--1")
+    .then(resp => {
+        // get
+        let $ = cheerio.load(resp.data);
+        //returns an object of objects of the first some number loaded items
+        let x = $(".product-container");
+        console.log("length of received products", x.length);
+        let prices = [];
+        let names = [];
+        let qtys = [];
+        // drag strings out of the objects retrieved
+        for (let i = 0; i < x.length; i++) {
+            // price
+            prices.push(x[i]
+                .children[0].children[0]
+                .children[1].children[0]
+                .children[0].children[0]
+                .children[0].children[0]
+                .data
+            );
+            // name
+            names.push(x[i]
+                .children[0].children[0]
+                .children[1].children[1]
+                .children[0].children[0]
+                .data
+            );
+            // qty
+            qtys.push(x[i]
+                .children[0].children[0]
+                .children[1].children[1]
+                .children[1].children[0]
+                .children[0].children[0]
+                .data
+            );
+        }
+
+        console.log(prices);
+        prices = prices.map(price => parseFloat(price.substring(1)));
+        console.log("original strings");
+        console.log(qtys);
+        // get rid of qty multipliers (e.g. "4 x 800g")
+        let qtyMults = qtys.map(qty => {
+            let split = qty.split(" ");
+            return split.length > 1 ? parseInt(split[0]) : 1;
+        });
+        let qtyNumbers = qtys.map(qty => parseFloat(qty.match(/\d+.?\d+/)[0]));
+        let qtyUnits = qtys.map(qty => qty.match(/[a-zA-Z]+$/)[0]);
+        // convert everything to grams because model enum no kg
+        qtyNumbers = qtyNumbers.map((qty, i) => {
+            return convert(qty * qtyMults[i]).from(qtyUnits[i]).to('g');
+        });
+        console.log("numbers only");
+        console.log(qtyNumbers);
+        let items = [];
+        // it's inefficient to go again here but nevermind lah
+        for (let i = 0; i < x.length; i++) {
+            items.push({
+                itemName: names[i],
+                price: prices[i],
+                quantity: qtyNumbers[i],
+                unit: "grams",
+                __v: 0
+            });
+        }
+
+
+        Item.deleteMany({})
+        .then(() => {
+            Item.create(items)
+                .then(created => {
+                    console.log(created);
+                    mongoose.disconnect(() => console.log("end"));
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        })
+
+        // todo: import Item model somewhere on top
+        // create a new Item() and save it for each item in 'items' array
+    })
+    .catch(err => console.log(err));
+
+
+
+// APPENDICES
 
 // request("https://www.fairprice.com.sg/category/infant-formula--1", (err, res, html) => {
 //     if (!err && res.statusCode == 200) { //200 successful http response
@@ -19,60 +133,10 @@ const writeStream = fs.createWriteStream("post.csv")
 //         writeStream.write(`${siteItem.text()} \n`)
 //     }
 // })
-
-request("https://www.fairprice.com.sg/category/infant-formula--1", (err, res, html) => {
-    if (!err && res.statusCode == 200) { //200 successful http response
-        // console.log(html);
-        let $ = cheerio.load(html);
-         // ITEM NAME
-        let siteContainer = $("div[data-testid=product]").children()
-        let items = []
-        let prices = []
-        
-        siteContainer.each(function(i){ 
-            items.push($("div[data-testid=product]").children().eq(i).children().eq(1).children().eq(0).text())
-        })
-        siteContainer.each(function(i){ 
-            prices.push($("div[data-testid=product]").children().eq(i).children().eq(0).children().eq(0).children().eq(0).text())
-        })
-
-        // fs.writeFile("output.txt", JSON.stringify(items, prices, null, 4), function(err){
-        //     if(err){
-        //         console.log(err);
-        //     } else (
-        //         console.log("data added to file");
-        //     )
-        // })
-        
-        
-        console.log(items);
-        console.log(prices);
-
-        
-
-    }
-    
-    
-})
-
-
-// axios will get the content
-// axios.get("https://www.fairprice.com.sg/category/infant-formula--1")
-//     .then(resp => {
-//         // get
-//         let $ = cheerio.load(resp.data)
-
-//         // for each of the product container..
-//         // for (let i = 0; i < 57; i++) {
-
-//             // let x = $(".product-container")[i] 
-
-//             // get price
-//             let price2 = $("div[data-testid=product]").children().eq(1).children().eq(0).children().eq(0).children().eq(0).text()
-//             console.log(price)
-
-//             //get item name
-//             let name = $("div[data-testid=product]").children().eq(1).children().eq(1).children().eq(0).text()
-//             console.log(name)
-//         // }
-//     }); 
+//     let result1 = $("div[data-testid=product]")
+//     .children().eq(i)
+//     .children().eq(1)
+//     .children().eq(0).text()
+// if (result1 !== "") {
+//     items.push(result1)
+// }
